c180ee91ac19925dd367d9f712d42d6050a91410
Croopter <fransoishoussin@gmail.com>
2022-07-10 02:17:31 +0200

    add input mode to popup window

--- src/ui.rs
+++ src/ui.rs
@@ -9,18 +9,27 @@ use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Alignment, Constraint, Direction, Layout, Rect},
     text::Text,
-    widgets::{Block, BorderType, Borders, Paragraph, Clear},
+    widgets::{Block, BorderType, Borders, Clear, Paragraph, Wrap},
     Frame, Terminal,
 };
 
+enum InputMode {
+    Normal,
+    Editing,
+}
+
 struct App {
     pub new_task_pop_up: bool,
+    pub input_mode: InputMode,
+    pub input: String,
 }
 
 impl App {
     pub fn new() -> Self {
         App {
             new_task_pop_up: false,
+            input_mode: InputMode::Normal,
+            input: String::new(),
         }
     }
 }
@@ -52,13 +61,43 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     let mut app = App::new();
     loop {
         terminal.draw(|f| ui(f, &mut app))?;
-
-        if let Event::Key(key) = event::read()? {
-            if let KeyCode::Char('q') = key.code {
-                return Ok(());
+        match app.input_mode {
+            InputMode::Normal => {
+                if let Event::Key(key) = event::read()? {
+                    if let KeyCode::Char('q') = key.code {
+                        return Ok(());
+                    }
+                    if let KeyCode::Char('n') = key.code {
+                        app.new_task_pop_up = true;
+                        app.input_mode = InputMode::Editing;
+                    }
+                }
             }
-            if let KeyCode::Char('n') = key.code {
-                app.new_task_pop_up = !app.new_task_pop_up;
+            InputMode::Editing => {
+                if let Event::Key(key) = event::read()? {
+                    match key.code {
+                        KeyCode::Char(c) => {
+                            app.input.push(c);
+                        }
+                        KeyCode::Backspace => {
+                            if app.input.len() > 0 {
+                                app.input.pop();
+                            }
+                        }
+                        KeyCode::Esc => {
+                            app.input_mode = InputMode::Normal;
+                            app.new_task_pop_up = false;
+                            app.input = String::new();
+                        }
+                        KeyCode::Enter => {
+                            //TODO add a tasklist to app
+                            // TODO tasklist.new(blabla) if !input.is_empty()
+                            app.input_mode = InputMode::Normal;
+                            app.new_task_pop_up = false;
+                        }
+                        _ => {}
+                    }
+                }
             }
         }
     }
@@ -76,15 +115,10 @@ fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     f.render_widget(command_helper(), chunks[1]);
 
     if app.new_task_pop_up {
-        // Todo wrap pop up widget in main widget
-        let block = Block::default()
-            .title("New task")
-            .title_alignment(Alignment::Center)
-            .border_type(BorderType::Rounded)
-            .borders(Borders::ALL);
-        let area = centered_rect(60, 20, f.size());
+        let mut area = centered_rect(60, 20, f.size());
+        area.height = 3;
         f.render_widget(Clear, area);
-        f.render_widget(block, area);
+        f.render_widget(input_pop_up(app), area);
     }
 }
 
@@ -125,21 +159,33 @@ fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
             [
                 Constraint::Percentage((100 - percent_y) / 2),
                 Constraint::Percentage(percent_y),
-                Constraint::Percentage((100 - percent_y) / 2)
+                Constraint::Percentage((100 - percent_y) / 2),
             ]
             .as_ref(),
         )
         .split(r);
-    
+
     Layout::default()
         .direction(Direction::Horizontal)
         .constraints(
             [
                 Constraint::Percentage((100 - percent_x) / 2),
                 Constraint::Percentage(percent_x),
-                Constraint::Percentage((100 - percent_x) / 2)
+                Constraint::Percentage((100 - percent_x) / 2),
             ]
-            .as_ref()
+            .as_ref(),
         )
         .split(popup_layout[1])[1]
+}
+
+fn input_pop_up(app: &App) -> Paragraph<'static> {
+    Paragraph::new(Text::raw(app.input.clone()))
+        .wrap(Wrap { trim: true })
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .border_type(BorderType::Rounded)
+                .title("Add a new task")
+                .title_alignment(Alignment::Center),
+        )
 }
